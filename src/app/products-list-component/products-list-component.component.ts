49e7e0b87cf78193705e9062ac88fc256d0f4789
arcanist123 <dmitry.yudin.a@gmail.com>
2019-04-07 02:01:09 +0200

    lasjkflka

--- src/app/products-list-component/products-list-component.component.ts
+++ src/app/products-list-component/products-list-component.component.ts
@@ -19,6 +19,7 @@ export class ProductsListComponentComponent {
 
 
   clicked(product: Product) {
+    // some comment
     this.currentProduct = product;
     this.productSelected.emit(product);
   }
